3112c8874b89ff60166bb23f787d56b1402ef18c
Josh <j.d.gray@outlook.com>
2024-04-11 20:33:42 -0700

    adding component interface

---
+++ GroceryList/Domain/IComponent.cs
@@ -0,0 +1,20 @@
+﻿namespace Domain
+{
+    public interface IComponent
+    {
+        string Name { get; }
+        decimal Quantity { get; }
+        Measurement Measurement { get; }
+
+    }
+
+    public enum Measurement
+    {
+        Each,
+        Quart,
+        Liter,
+        Gallon,
+        Cup,
+        Box
+    }
+}
--- GroceryList/Domain/IItem.cs
+++ GroceryList/Domain/IItem.cs
@@ -4,7 +4,6 @@
     {
         public string Name { get; set; }
         public string Description { get; set; }
-
         public ItemType Type { get; set; }
 
     }
